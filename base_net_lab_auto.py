c6931b798a570eea294060695e1c2932c67b6208
CurtisJTrobaugh <jack.trobaugh@gmail.com>
2016-05-22 19:06:02 -0400

    Base Script

---
+++ base_net_lab_auto.py
@@ -0,0 +1,36 @@
+"""  Network Automation Script  """
+
+class NetworkDevice(object):
+    """
+    Class that defines the major elements of a network device.
+    """
+    pass
+
+class SSHClientConnection(object):
+    """
+    Creates an SSH Connection channel.  Defines methods that will be
+    used on the channel.
+
+    Create channel
+    Prepare interactive shell
+    Send commands to the shell
+    Parse output from the shell
+    """
+
+    pass
+
+def linear_two_switch(args):
+    """Creates linear topology with two switches"""
+    pass
+
+def linear_three_switch(args):
+    """Creates linear topology with three switches"""
+
+def linear_four_switch(args):
+    """Creates linear topology with four switches"""
+
+def two_teir_three_switch(args):
+    """Creates two tier topology with three switches"""
+
+def two_tier_four_switch(args):
+    """Creates two tier topology with four switches"""
