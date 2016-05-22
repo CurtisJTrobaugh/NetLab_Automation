70ced377b33fed9a751140c24344e3c00fde1175
CurtisJTrobaugh <jack.trobaugh@gmail.com>
2016-05-22 19:07:08 -0400

    Update base_net_lab_auto.py

--- base_net_lab_auto.py
+++ base_net_lab_auto.py
@@ -25,12 +25,16 @@ def linear_two_switch(args):
 
 def linear_three_switch(args):
     """Creates linear topology with three switches"""
+    pass
 
 def linear_four_switch(args):
     """Creates linear topology with four switches"""
+    pass
 
 def two_teir_three_switch(args):
     """Creates two tier topology with three switches"""
+    pass
 
 def two_tier_four_switch(args):
     """Creates two tier topology with four switches"""
+    pass
